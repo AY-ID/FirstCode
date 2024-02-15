9cfef976de76dbaedcd34861abe757b9f4efd72c
Ayid <ayo.daniyan@outlook.com>
2024-02-15 17:13:12 +0000

    first commit

---
+++ first.py
@@ -0,0 +1,73 @@
+#name="James"
+#firstname='Luke'
+
+#age=40
+# firstName= camel notation hbghjbgjhhghg
+#last_name="Kyron"
+#adult=True
+#Sex='Male'
+
+#Data types integer string, boolean,decimal
+#print(firstname,age,adult,Sex)
+#if 56 > 21:
+  ##print('yes')
+#x = "awesome"
+#def myfunc():
+   # x = "fantastic"
+    #def myfunc():
+        ##x = "sweet"
+        #def myfunc():
+            ##x = 'Python'    
+            ##print('I love '+ x)
+        
+        #myfunc()
+        #print('Python is '+ x)    
+    #myfunc()
+    #print('Python is '+ x)
+        
+#myfunc()
+#print('Python is '+ x)
+x = 'Darwin'
+y = 'Rome'
+z = '28'
+#print('My Name is '.upper()  + x.upper())
+#print('I am ' + z + ' years Old'.upper())
+#print('I live in ' +  y.upper())
+#print(y + ' is a nice place'.upper())
+#print('James\nBond')
+#print(x.replace('Darwin', 'Mike'))
+print(x[2:1])
+
+def addNumbers(number1,number2):
+    answer=number1+number2
+    return answer
+
+
+print(addNumbers(30,60))
+
+
+print(addNumbers(100,600))
+
+myname="james luke"
+print(myname.capitalize())
+
+
+def sayHello(name):
+    print("Good afternoon " ,name)
+
+
+
+sayHello("Mary")
+
+sayHello("Davi")
+
+
+print(addNumbers(100,900))
+
+age=71
+if(age<=18):
+    print("You are a child")
+elif (age>18 and age<70):
+    print("You aree an adult")
+elif (age>70):
+    print("You are an old man")
